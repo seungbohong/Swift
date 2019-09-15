5b30aace8d9c8b98507fa1238dde9495d6645edb
seungbohong <seungbohong94@gmail.com>
2019-09-16 00:52:12 +0900

    practicing auto layout

---
+++ auto_layout/auto_layout/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  auto_layout
+//
+//  Created by 홍승보 on 15/09/2019.
+//  Copyright © 2019 Seung-Bo Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // here's our entry point into our app
+        // view.backgroundColor = .yellow
+        
+    }
+    
+    
+
+}
+
